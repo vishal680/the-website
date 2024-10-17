1dcbbc079c79a32d565187b3ea65646e7228b26b
Vishal Chauhan <vishal680@users.noreply.github.com>
2024-10-17 14:34:21 +0530

    improved the code

---
+++ app.js
@@ -0,0 +1,7 @@
+const hamburgerToggle = document.querySelector("#hamburger-toggle");
+
+hamburgerToggle.addEventListener("click", onHamburgerClick);
+
+function onHamburgerClick() {
+    alert("click");
+}
